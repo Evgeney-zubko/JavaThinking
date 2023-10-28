f68d6f22f0b7e5f6bfbbb134536491903fc01cea
ezubko <zubko_evgeney@mail.ru>
2023-10-28 19:59:47 +0300

    1 задание выполнено

---
+++ src/java/main/com.example/P93_ex_1.java
@@ -0,0 +1,11 @@
+package main.com.example;
+
+public class P93_ex_1 {
+    int a;
+    char b;
+    public static void main(String[] args) {
+        P93_ex_1 example = new P93_ex_1();
+        System.out.println(example.a+example.b);
+
+    }
+}
